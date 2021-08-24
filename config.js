3261c1e455f14a2ac98de8aef16961d9b53d179e
Grzegorz Celek <g.celek@gmail.com>
2021-08-24 14:10:20 +0200

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:admin@cluster0.skvbs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
